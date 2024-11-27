7a86493cbf227ebc0415c28bc4be7e3f1da22786
ethan-1003 <ethan@chtlab.io>
2024-11-27 11:55:38 +0700

    a

--- addon/run.py
+++ addon/run.py
@@ -1,7 +1,13 @@
 import lgpio
+from time import sleep
+import lgpio
 
-h = lgpio.gpiochip_open(0)  # Mở GPIO chip 0
-lgpio.gpio_claim_output(h, 17)  # Đặt chân GPIO 17 làm output
-lgpio.gpio_write(h, 17, 1)  # Bật chân GPIO 17
-lgpio.gpiochip_close(h)  # Đóng chip
-print("GPIO 17 đã bật!")
+h = lgpio.gpiochip_open(0)
+lgpio.gpio_claim_output(h,17)
+while True:
+ lgpio.gpio_write(h, 17, 1)  # Bật chân GPIO 17  
+ print("GPIO 17 đã bật!")
+ sleep(2)
+ lgpio.gpio_write(h,17,0)
+ print("GPIO 17 da tat")
+ sleep(2)
